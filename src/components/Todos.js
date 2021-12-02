9494f0642fd0ef2b5d11c99eba2b75fa345714e4
aginamena5 <aginamena5@gmail.com>
2021-12-02 03:57:03 -0500

    removed add and drop feature

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import "../styles/Todos.scss";
 import TodoItem from './TodoItem';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 
 function Todos(props) {
@@ -30,8 +30,8 @@ function Todos(props) {
                         alt="empty-illustration" id="emptyTodoList" />
                 }
 
-            </div>
 
+            </div>
             <div id="todoButtons"
                 style={props.background === "sun" ? { borderTop: "1px solid #E3E4F1" } :
                     { borderTop: "1px solid #393A4B" }}
@@ -40,18 +40,24 @@ function Todos(props) {
                     props.listOfTodos.length == 0 ? <small className="utils">Empty List</small> :
                         <small className="utils">{props.listOfTodos.length} item(s) left</small>
                 }
-                <small className="listOfOptions"
-                    style={props.currentTodo === "All" ? { color: "#3A7CFD" } : { color: "#9495A5" }}
-                    onClick={() => update("", "GET", "All")}
-                >All</small>
-                <small className="listOfOptions"
-                    style={props.currentTodo === "Active" ? { color: "#3A7CFD" } : { color: "#9495A5" }}
-                    onClick={() => update("active", "GET", "Active")}
-                >Active</small>
-                <small className="listOfOptions"
-                    style={props.currentTodo === "Completed" ? { color: "#3A7CFD" } : { color: "#9495A5" }}
-                    onClick={() => update("completed", "GET", "Completed")}
-                >Completed</small>
+                <div
+
+                    style={props.background === "sun" ? { backgroundColor: "white" } :
+                        { backgroundColor: "rgb(37, 39, 61)" }}
+                >
+                    <small className="listOfOptions"
+                        style={props.currentTodo === "All" ? { color: "#3A7CFD" } : { color: "#9495A5" }}
+                        onClick={() => update("", "GET", "All")}
+                    >All</small>
+                    <small className="listOfOptions"
+                        style={props.currentTodo === "Active" ? { color: "#3A7CFD" } : { color: "#9495A5" }}
+                        onClick={() => update("active", "GET", "Active")}
+                    >Active</small>
+                    <small className="listOfOptions"
+                        style={props.currentTodo === "Completed" ? { color: "#3A7CFD" } : { color: "#9495A5" }}
+                        onClick={() => update("completed", "GET", "Completed")}
+                    >Completed</small>
+                </div>
                 <small className="utils"
                     style={props.currentTodo === "Clear-Completed" ? { color: "#3A7CFD" } : { color: "#9495A5" }}
                     onClick={() => update("clearAllCompleted", "DELETE", "Clear-Completed")}>Clear Completed</small>
